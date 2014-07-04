d57d0a8ced5e6c9bf97de68350adb96c970db6aa
duhaitao <duhaitao1974@gmail.com>
2014-07-04 15:47:26 +0800

    mod 01.go 02.go

--- 01.go
+++ 01.go
@@ -1,35 +1,40 @@
 package main
 
 import (
-	"net"
-	"log"
-	"fmt"
 	"encoding/binary"
+	//"fmt"
+	"log"
+	"net"
 )
 
-func main () {
-	ifi, err := net.InterfaceByName ("eth0")
+func main() {
+	ifi, err := net.InterfaceByName("eth0")
 	if err != nil {
-		log.Fatal ("eth0 err")
+		log.Fatal("eth0 err")
 	}
 
-	multicastip := net.ParseIP ("230.1.1.1")
-	pUDPAddr := &net.UDPAddr {IP: multicastip, Port: 12345}
+	multicastip := net.ParseIP("230.1.1.1")
+	pUDPAddr := &net.UDPAddr{IP: multicastip, Port: 12345}
 	// fmt.Println (*pUDPAddr)
-	conn, err := net.ListenMulticastUDP ("udp4", ifi, pUDPAddr)
+	conn, err := net.ListenMulticastUDP("udp4", ifi, pUDPAddr)
 	if err != nil {
-		log.Fatal ("net.ListenMulticastUDP err")
+		log.Fatal("net.ListenMulticastUDP err")
 	}
 
-	buf := make ([]byte, 4096)
+	buf := make([]byte, 4096)
 	for {
-		length, _, err := conn.ReadFromUDP (buf)
+		_, _, err := conn.ReadFromUDP(buf)
 		if err != nil {
-			log.Fatal ("ReadFromUDP err")
+			log.Fatal("ReadFromUDP err")
 		}
 
-		fmt.Println ("type: ", binary.BigEndian.Uint16 (buf[:2]))
-		fmt.Println ("len: ", binary.BigEndian.Uint32 (buf[2:6]))
-		fmt.Println (string(buf[6:length]))
+		//fmt.Println("type: ", binary.BigEndian.Uint16(buf[:2]))
+		//fmt.Println("len: ", binary.BigEndian.Uint32(buf[2:6]))
+		//fmt.Println("seq: ", binary.BigEndian.Uint32(buf[6:10]))
+		//fmt.Println(string(buf[10:length]))
+		seq := binary.BigEndian.Uint32(buf[6:10])
+		if seq % 10000 == 0 {
+			log.Fatal (seq)
+		}
 	}
 }
--- 02.go
+++ 02.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	"net"
-	"log"
-	"fmt"
 	"encoding/binary"
+	//"fmt"
+	"log"
+	"net"
 )
 
-func main () {
+func main() {
 	/// conn, err := net.Dial ("udp", "224.0.0.1:12345")
-	conn, err := net.Dial ("udp", "230.1.1.1:12345")
+	conn, err := net.Dial("udp", "230.1.1.1:12345")
 	if err != nil {
-		log.Fatal ("dial err")
+		log.Fatal("dial err")
 	}
 
-	buf := make ([]byte, 1024)
+	buf := make([]byte, 1024)
 
 	/*   +------------------------+
-	 *   | type | len | val       |
+	 *   | type | len | seq | val |
 	 *   +------------------------+
-	 *     2B     4B  
+	 *     2B     4B    4B
 	 */
-	var pkttype uint16 = 2
-	binary.BigEndian.PutUint16 (buf[:2], pkttype)
-	var length uint32 = 10
-	binary.BigEndian.PutUint32 (buf[2:6], length)
-	copy (buf[6:], []byte("0123456789"))
+	var seq uint32
+	for {
+		var pkttype uint16 = 2
+		binary.BigEndian.PutUint16(buf[:2], pkttype)
+		var length uint32 = 10
+		binary.BigEndian.PutUint32(buf[2:6], length)
 
-	fmt.Println (string(buf))
-	_, err = conn.Write (buf[:16])
-	conn.Close ()
+		copy(buf[6:], []byte("0123456789"))
+		seq++
+
+		binary.BigEndian.PutUint32(buf[6:10], seq)
+
+		//fmt.Println(string(buf))
+		_, err = conn.Write(buf[:20])
+	}
+	conn.Close()
 }
