871e9d7daa20bb7d4b7abc4535cd340ea5d88545
duhaitao <duhaitao1974@gmail.com>
2014-07-15 18:16:56 +0800

    mod

--- rmcast/rqueue.go
+++ rmcast/rqueue.go
@@ -6,19 +6,17 @@ import (
 	"log"
 )
 
+type LostSeqInfo struct {
+	Begin, Count uint32
+}
+
 type Rqueue struct {
 	lst *list.List
+	lostSeq []LostSeqInfo
 }
 
 func NewRqueue () *Rqueue {
-/*
-	rqueue := new (Rqueue)
-	lst := list.New ()
-	rqueue.lst = lst
-	return rqueue
-*/
-	log.Println ("NewRqueue")
-	return &Rqueue {lst: list.New ()}
+	return &Rqueue {lst: list.New (), lostSeq: make([]LostSeqInfo, 160)}
 }
 
 /*
@@ -27,7 +25,6 @@ func NewRqueue () *Rqueue {
  */
 func (pqueue *Rqueue) Enque (pkg *PKG) {
 	insert_seq := pkg.GetSeq ()
-	/// log.Println ("insert seq: ", insert_seq)
 	var seq uint32
 	// find the proper place to enque it
 	for e := pqueue.lst.Front (); e != nil; e = e.Next () {
@@ -58,4 +55,32 @@ func (pqueue *Rqueue) First () *PKG {
 	}
 
 	return pqueue.lst.Front ().Value.(*PKG)
+}
+
+func (pqueue *Rqueue) GetLostSeqInfo (last_rcv_seq uint32) []LostSeqInfo {
+	var i int
+	begin_seq := last_rcv_seq
+
+	for e := pqueue.lst.Front (); e != nil; e = e.Next () {
+		seq := e.Value.(*PKG).GetSeq ()
+
+		if seq <= last_rcv_seq {
+			log.Fatal ("unorder seq: ", seq, "last_rcv_seq: ", last_rcv_seq)
+		}
+
+		if seq == last_rcv_seq + 1 {
+			log.Fatal ("unorder seq: ", seq, "last_rcv_seq: ", last_rcv_seq)
+		}
+
+		if i == 0 {
+			pqueue.lostSeq[i].Begin = last_rcv_seq + 1
+			pqueue.lostSeq[i].Count = seq - last_rcv_seq - 1
+		} else {
+			pqueue.lostSeq[i].Begin = pqueue.lostSeq[i - 1].Begin + pqueue.lostSeq[i - 1].Count + 1 + 1
+			pqueue.lostSeq[i].Count = seq - pqueue.lostSeq[i - 1].Begin - 1
+		}
+		i++
+	}
+
+	return pqueue.lostSeq[:i - 1]
 }
